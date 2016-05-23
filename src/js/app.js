0ff233c8e0737f36cd3bbfe759438057ba8b1857
jasondippel <dippelj12@gmail.com>
2016-05-23 11:01:26 -0400

    Add index route for initial page

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ import { Router, Route, IndexRoute, HashHistory } from "react-router";
 
 // Pages
 import Layout from "./pages/Layout";
+import About from "./pages/About";
 import Registration from "./pages/Registration";
 import SignIn from "./pages/SignIn";
 
@@ -16,6 +17,7 @@ const app = document.getElementById('app');
 ReactDOM.render(
   <Router history={HashHistory}>
     <Route path="/" component={Layout}>
+      <IndexRoute component={About}></IndexRoute>
       <Route path="register" name="registration" component={Registration}></Route>
       <Route path="signIn" name="signIn" component={SignIn}></Route>
     </Route>
