1d173acad346df02ca18bf47d079fb72131a55d8
jasondippel <dippelj12@gmail.com>
2017-03-23 19:56:48 -0400

    I really wish I choose a different list component to use... fixing more bugs with it

--- src/leaguespot-components/components/lists/List.jsx
+++ src/leaguespot-components/components/lists/List.jsx
@@ -8,14 +8,41 @@ import './List.less';
 /* Script Dependencies */
 import React from 'react';
 import _ from 'underscore';
-import { AutoSizer, List as VirtualList } from 'react-virtualized';
+import { AutoSizer, List as WrappedList } from 'react-virtualized';
 
+class VirtualList extends React.Component {
+  componentDidUpdate(prevProps) {
+    console.log('jason tset23');
+    if (this.list && prevProps.renderData !== this.props.renderData) this.list.forceUpdateGrid()
+  }
+
+  render() {
+    const {rowRenderer, renderData, ...props} = this.props
+    return (
+      <WrappedList
+        {...props}
+        ref={c => this.list = c}
+        rowRenderer={props => rowRenderer({...props, ...renderData})}
+      />
+    )
+  }
+}
 
 export default class List extends React.Component {
   constructor(props) {
     super(props);
 
     this.renderItem = this.renderItem.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    let test1 = this.list;
+    let test2 = this.List;
+    let test3 = this.VirtualList;
+    console.log('jason test');
+    if (this.list && prevProps.items !== this.props.items) {
+      this.list.forceUpdateGrid();
+    }
   }
 
   /**
